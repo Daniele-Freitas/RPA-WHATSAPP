27efcb9b040039c99fb56859fef16d0d87284d29
Daniele Freitas <danielefreitas2006df@gmail.com>
2026-06-03 18:21:14 -0300

    feat(frontend): add routes for Dashboard and CriacaoCampanha

--- RPA-FRONT/src/app/app.routes.ts
+++ RPA-FRONT/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		title: 'Dashboard',
+		loadComponent: () => import('./features/dashboard/dashboard').then((m) => m.Dashboard),
+	},
+	{
+		path: 'criacao',
+		title: 'Criação de Campanha',
+		loadComponent: () => import('./features/criacao-campanha/criacao-campanha').then((m) => m.CriacaoCampanha),
+	},
+	{ path: '**', redirectTo: '' },
+];
